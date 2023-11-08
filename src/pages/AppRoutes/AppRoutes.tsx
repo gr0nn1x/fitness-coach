c71cd618e666725025e5f0975b28a2cd82f1fc75
gr0nn1x <fanyfany888@gmail.com>
2023-11-08 14:37:17 +0100

    gg

---
+++ src/pages/AppRoutes/AppRoutes.tsx
@@ -0,0 +1,13 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { MainPage } from "../MainPage/MainPage";
+
+export function AppRoutes() {
+  return (
+    <HashRouter>
+      <Routes>
+        
+        <Route path="/" element={<MainPage/>}/>
+        </Routes>
+    </HashRouter>
+  );
+}
